3a688b339443ab37461388cf7774cb1d17b1832e
Datsudo <ekflfdv29@gmail.com>
2022-11-29 14:05:21 +0800

    Add procedures

--- dat_iters.py
+++ dat_iters.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 
+ITERATION_LIMIT = 10000
+
+
 class DatIters:
 
     def __init__(self, matrix_shape: int, 
@@ -9,28 +12,34 @@ class DatIters:
 
         self.matrix: np.matrix = matrix
         self.constants: np.matrix = constants
-        self.diag_inverse: np.matrix = np.linalg.inv(np.diag(np.diag(matrix)))
-        self.init_values: np.matrix = np.matrix([[2] for _ in range(matrix_shape)])
-        self.tolerance: np.float128 = 1e-16
-        self.error: np.float128 = 1
+        self.diag = np.diag(np.diag(matrix))
+        self.diag_inverse: np.matrix = np.linalg.inv(self.diag)
+        self.init_values: np.matrix = np.matrix(
+                [[2] for _ in range(matrix_shape)]
+        )
+        # self.tolerance: float = 1e-15
+        # self.error: float = 1.0
 
     def jacobi(self) -> np.matrix:
-        while self.error > self.tolerance:
+        for _ in range(ITERATION_LIMIT):
             dx = self.diag_inverse * (
                     self.constants - self.matrix * self.init_values)
             next_values = self.init_values + dx
             
-            self.error = max(map(max, abs(dx / self.init_values)))
-
             self.init_values = next_values
 
         return next_values
 
     def gauss_seidel(self) -> np.matrix:
-        while self.error > self.tolerance:
-            pass
+        for _ in range(ITERATION_LIMIT):
+            tri_lower_inverse = np.linalg.inv(np.tril(self.matrix))
+            tri_upper = np.triu(self.matrix - self.diag)
 
-    def is_diag_dominant(self) -> bool:
-        pass
+            next_values = tri_lower_inverse * (
+                    self.constants - tri_upper * self.init_values)
+
+            self.init_values = next_values
+
+        return next_values
 
 
