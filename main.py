1c005ea54cb1563b453f286c7301da9a39ed17e3
Datsudo <ekflfdv29@gmail.com>
2022-11-29 22:50:26 +0800

    Handle input errors

--- main.py
+++ main.py
@@ -1,21 +1,50 @@
-import numpy as np
-import re
-
 from dat_iters import *
+
+import re
+import sys
+
+# Try importing numpy
+try:
+    import numpy as np
+except ImportError:
+    print(
+    """
+    --- Install first the required package: numpy
+    --- To install it run: python -m pip install numpy
+    --- If that doesn't work, try replacing 'python' with 'python3' or 'pip' with 'pip3'
+    """
+    )
+
+    # Exit program when numpy isn't installed
+    sys.exit(0)
 
 
 def main() -> None:
-    matrix_shape = int(input("Matrix shape: "))
+    print(INSTRUCTION)
 
+    # Get matrix shape
+    while True:
+        matrix_shape = input("Matrix shape: ")
+        if matrix_shape.isnumeric() and int(matrix_shape) >= 1:
+            matrix_shape = int(matrix_shape)
+            break
+        else:
+            print("--- Input is not an integer ---")
+
+    # Initialize matrix
     matrix = np.zeros(shape=(matrix_shape, matrix_shape))
+    # Initialize matrix for constants
     constants = np.zeros(shape=(matrix_shape, 1))
 
+    # Get coefficients for each equation/row
     for r in range(matrix_shape):
-        print("-"*30)
+        print("-"*63)
         while True:
             new_row = list(map(float, input(f"Row {r+1}: ").split(" ")))
+
+            # Check if diagonally dominant
             if abs(new_row[r]) >= sum([abs(n) for n in (new_row[:r] + new_row[r+1:])]) and \
-                    len(new_row) == matrix_shape:
+                    len(new_row) == matrix_shape: # Check if input is compatible with matrix shape
                 matrix[r] = new_row
                 break
             else:
@@ -30,22 +59,23 @@ def main() -> None:
 
     iter = DatIters(matrix_shape, matrix, constants)
 
-    ans1 = iter.jacobi()
-    ans2 = iter.gauss_seidel()
+    jacobi_result = iter.jacobi()
+    gauss_seidel_result = iter.gauss_seidel()
 
-    print("-"*30)
+    # Print results
+    print("="*63)
     print(f"JACOBI:\n")
     for i in range(matrix_shape):
         print(f"x_{i+1} = ", end="")
-        print(re.sub('[\[\]]', '', np.array_str(ans1[i][0])))
+        print(re.sub('[\[\]]', '', np.array_str(jacobi_result[i][0])))
 
-    print("-"*30)
+    print("-"*63)
     print(f"GAUSS-SEIDEL:\n")
     for i in range(matrix_shape):
         print(f"x_{i+1} = ", end="")
-        print(re.sub('[\[\]]', '', np.array_str(ans2[i][0])))
+        print(re.sub('[\[\]]', '', np.array_str(gauss_seidel_result[i][0])))
 
-    print("-"*30)
+    print("="*63)
 
 
 if __name__ == "__main__":
